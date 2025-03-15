97ed399652ba774d5af73496342b7c7268e9fb98
FrankenFreak <64947352+FrankenFreak@users.noreply.github.com>
2025-03-15 14:50:51 +0000

    update

--- minijam-refresh/Assets/CliveScript/Planet.cs
+++ minijam-refresh/Assets/CliveScript/Planet.cs
@@ -10,9 +10,12 @@ public class Planet : MonoBehaviour
     public float maxMass = 10f;
     public float minRadius = 1f;
     public float maxRadius = 10f;
+    public float minG = 1f;
+    public float maxG = 10f;
 
     public float mass { get; private set; }
     public float radius { get; private set; }
+    public float G { get; private set; }
 
     // List of all planets
     public static List<Planet> allPlanets = new List<Planet>();
@@ -23,12 +26,16 @@ public class Planet : MonoBehaviour
         // Set random mass and radius of planet
         mass = Random.Range(minMass, maxMass);
         radius = Random.Range(minRadius, maxRadius);
+        G = Random.Range(minG, maxG);
 
         // Set scale of planet
         transform.localScale = new Vector3(radius * 2, radius * 2, radius);
 
         // List all planets
-        Debug.Log(allPlanets);
+        //foreach (Planet planet in allPlanets)
+        //{
+        //    Debug.Log("Planet Mass: " + planet.mass + " Radius: " + planet.radius + " G: " + planet.G);
+        //}
     }
 
    

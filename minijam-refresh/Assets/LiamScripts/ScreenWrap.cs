8d735b306899c98460c730b318db384c26db61aa
sx19801 <sx19801@bristol.ac.uk>
2025-03-16 14:56:41 +0000

    screen wrap added

--- minijam-refresh/Assets/LiamScripts/ScreenWrap.cs
+++ minijam-refresh/Assets/LiamScripts/ScreenWrap.cs
@@ -7,6 +7,27 @@ public class ScreenWrap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        float rightSideOfScreen = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)).x;
+        float leftSideOfScreen = Camera.main.ScreenToWorldPoint(new Vector2(0f,0f)).x;
         
-    }
+        float topOfScreen = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)).y;
+        float bottomOfScreen = Camera.main.ScreenToWorldPoint(new Vector2(0f,0f)).y;
+        
+        if (screenPos.x <= 0 && astronaut.linearVelocity.x < 0){
+            transform.position = new Vector2(rightSideOfScreen, transform.position.y);
+        }
+        else if (screenPos.x >= Screen.width && astronaut.linearVelocity.x > 0){
+            transform.position = new Vector2(leftSideOfScreen, transform.position.y);
+        }
+        else if (screenPos.y >= Screen.height && astronaut.linearVelocity.y > 0){
+            transform.position = new Vector2(transform.position.x, bottomOfScreen);
+        }
+        else if (screenPos.y <= 0 && astronaut.linearVelocity.y < 0){
+            transform.position = new Vector2(transform.position.x, topOfScreen);
+        }
+
+        
+    }   
 }
